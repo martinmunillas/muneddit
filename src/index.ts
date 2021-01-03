d208c5b59856bf4e94f68667796b396497288d86
Martin Munilla <martinmunillas@gmail.com>
2021-01-03 13:13:53 -0300

    💎 GraphQL Set up

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
 import { MikroORM } from '@mikro-orm/core';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
 
 import ormConfig from './mikro-orm.config';
-import { Post } from './entities/Post';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
   const orm = await MikroORM.init(ormConfig);
   await orm.getMigrator().up();
-//   const post = orm.em.create(Post, { title: 'First Post ever' });
-//   await orm.em.persistAndFlush(post);
 
-//   const posts = await orm.em.find(Post, {})
-//   console.log(posts)
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(3000, () => {
+    console.log(3000);
+  });
 };
 
 main();
